83613f1bf8e359d20d121f782c467bd695ca41b6
Kirill Custom <orehovsky@nppsatek.ru>
2026-02-28 19:41:33 +0300

    Initial Commit

---
+++ DockToggle/DockToggleApp.swift
@@ -0,0 +1,17 @@
+//
+//  DockToggleApp.swift
+//  DockToggle
+//
+//  Created by Кирилл Ореховский on 28.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct DockToggleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
